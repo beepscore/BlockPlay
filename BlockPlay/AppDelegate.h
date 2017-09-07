6de7570f28a6bdd6576c41b496bb2d4361d58435
beepscore <support@beepscore.com>
2017-09-06 18:37:23 -0700

    Initial Commit

---
+++ BlockPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockPlay
+//
+//  Created by Steve Baker on 9/6/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlockPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockPlay
+//
+//  Created by Steve Baker on 9/6/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
