dc7209282d4ffe3d24f9a6e5a7c5eef09d5cae63
beepscore <support@beepscore.com>
2017-09-20 23:40:57 -0700

    Add BSOtherVC. TODO: Set as initial view controller, embed in navigation controller, and add segue from BSOtherVC to BSViewController

---
+++ BlockPlay/BSOtherVC.h
@@ -0,0 +1,13 @@
+//
+//  BSOtherVC.h
+//  BlockPlay
+//
+//  Created by Steve Baker on 9/20/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSOtherVC : UIViewController
+
+@end
---
+++ BlockPlay/BSOtherVC.m
@@ -0,0 +1,41 @@
+//
+//  BSOtherVC.m
+//  BlockPlay
+//
+//  Created by Steve Baker on 9/20/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import "BSOtherVC.h"
+
+@interface BSOtherVC ()
+
+@end
+
+@implementation BSOtherVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+    NSLog(@"BSOtherVC dealloc");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
